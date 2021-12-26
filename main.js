09e4e1a57444bc96a8f042624fbd3e1bfd07b6d7
Samuel Yakubu <samuelyakubu737@gmail.com>
2021-12-26 22:32:31 +0100

    initial-commit

---
+++ main.js
@@ -0,0 +1,150 @@
+
+const baseUrl = 'https://api.ipify.org/?format=json';
+
+//GET DEFAULT IP ADDRESS 
+ fetch (baseUrl)
+    .then( response => response.json())
+    .then (data => { 
+        inputSpace.value = data.ip 
+        getPositionDetails (data.ip)
+
+}).catch (err => { 
+    console.log(err,'Error fetching ip address')
+})
+    
+const inputSpace = document.querySelector('.ip-value')  ;
+
+const submitBtn= document.querySelector("#properties > form > button") 
+submitBtn.addEventListener('click' , (e)=> { 
+    e.preventDefault()  ; //DISABLE DEFAULT EVENT ON SUBMIT BTN 
+})
+
+
+
+let ipAddress , timeZone,ISP , locality ; 
+
+const getPositionDetails = (ip) => {     
+const yek= 'at_FwAPdWr1oSWqo8xglR3MBEMHgeJ37' ; 
+const apiUrl =  `https://geo.ipify.org/api/v2/country,city?apiKey=${yek}&ipAddress=${ip}`   ; 
+
+fetch ( apiUrl )
+.then( response => response.json())
+.then (data => { 
+
+    const ipAddress = data.ip ; 
+    const zone = data.location.timezone ; 
+    const isp = data.isp ;
+    const locality = data.location.country + ', ' + data.location.region ;  
+    const lat = data.location.lat  ; 
+    const lng = data.location.lng ; 
+
+    updateDetails(ipAddress, zone , isp  , locality   )
+
+    plotGraph(lat , lng) 
+})
+.catch (err  => { 
+    console.log('Error at getting position')
+})
+
+}
+
+//FUNCTION TO PLOT GRAP USING ---API
+/* const plotGraph  = function (lat , lng  ) {  
+    try { 
+        console.log(lat , lng )
+
+        let  map = L.map('map').setView([lat , lng ], 0 );
+
+       let tileLayer =  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hpZWZjaXBoZXIiLCJhIjoiY2t4bHVpMmZoMWs2OTJ3dWI3ZnM3dG1qNSJ9.vT1pLDVvVIzncKD5aGqA9g', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiY2hpZWZjaXBoZXIiLCJhIjoiY2t4bHVpMmZoMWs2OTJ3dWI3ZnM3dG1qNSJ9.vT1pLDVvVIzncKD5aGqA9g' , 
+    }).addTo(map);
+    
+    const Icon = L.icon({
+        iconUrl: 'images/icon-location.svg',
+        // shadowUrl: 'images/icon-location.svg',
+    
+        iconSize:     [38, 95], // size of the icon
+        // shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        // shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    
+    L.marker([lat, lng ], {icon: Icon}).addTo(map);
+        
+
+    } 
+    catch (err) { 
+        console.log(err , 'at plot graph ')
+    }
+    
+} */
+
+
+//FUNCTION TO PLOT GRAP USING ---API
+let plotGraph  = function (lat , lng  ) {  
+    try { 
+
+        let map = L.map('map').setView([lat , lng ], 13 );
+        let tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hpZWZjaXBoZXIiLCJhIjoiY2t4bHVpMmZoMWs2OTJ3dWI3ZnM3dG1qNSJ9.vT1pLDVvVIzncKD5aGqA9g', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiY2hpZWZjaXBoZXIiLCJhIjoiY2t4bHVpMmZoMWs2OTJ3dWI3ZnM3dG1qNSJ9.vT1pLDVvVIzncKD5aGqA9g' , 
+    }).addTo(map);
+    
+    const Icon = L.icon({
+        iconUrl: 'images/icon-location.svg',
+        iconSize:     [38, 95], // size of the icon
+    });
+    
+    L.marker([lat, lng ], {icon: Icon}).addTo(map);
+        
+
+    } 
+    catch (err) {  
+      document.querySelector('#err').innerHTML = err + 'error at plot graph '
+    }
+    
+} 
+
+
+//FUNCTION TO UPDATE WRIGHTEN WRITEUPS AT PAGE 
+const updateDetails = (ip , zone , isp , locality ) => {  
+    try { 
+        document.querySelector('.db__content--ip ').innerHTML = ip 
+        document.querySelector('.db__content--zone span').innerHTML = zone
+        document.querySelector('.db__content--isp ').innerHTML = isp 
+        document.querySelector('.db__content--location ').innerHTML = locality
+    }
+    catch (err ) { 
+        console.log(err , 'at updateDetails' )
+    }
+    
+
+}
+
+
+
+//FUNCTION TO HIDE DISPLAY BOX ON HOVER TO MAP 
+
+const toggleDisplay = ()=> {  
+    const propertiesBlock = document.querySelector('.properties__display') ;  
+    
+    document.querySelector('#map-section').addEventListener('mouseover' , ()=> { 
+        propertiesBlock.classList.remove('active')
+    })
+    document.querySelector('#map-section').addEventListener('mouseout' , ()=> { 
+        propertiesBlock.classList.add('active')
+    })
+}
+
+toggleDisplay()
+
